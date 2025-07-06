25ed1968057f215dd70d833d81b8e053cea58802
GussGtz <gutierrezgustavocanul@gmail.com>
2025-07-05 23:00:12 -0500

    backend configurado mia

--- admin.js
+++ admin.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2'); // o mysql2/promise si usas promesas
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'root',
   database: 'formulario_db'
 });
 
