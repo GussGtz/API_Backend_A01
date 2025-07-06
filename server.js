71bd4741f23bc471f9fcc014bac31f3d8b738dc1
GussGtz <gutierrezgustavocanul@gmail.com>
2025-07-05 23:42:33 -0500

    Mejora en configuración CORS para Netlify

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const jwt = require('jsonwebtoken');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://lambent-mermaid-3d3361.netlify.app/', 
+}));
+
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
